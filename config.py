9c91e56548c4c21bd16136f04de71cfd90d974c0
buzz <dnaroid@gmail.com>
2019-05-08 12:50:25 +0300

    upd

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
+    print(os.environ)
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
